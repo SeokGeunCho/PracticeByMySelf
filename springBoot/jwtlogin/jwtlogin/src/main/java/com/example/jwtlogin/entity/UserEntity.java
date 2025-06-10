867ba58bbc87c8867c93095bd79fde9f5fc181af
seokgeun_cho <choseokgeun@gmail.com>
2025-06-10 17:51:22 +0900

    로그인, 회원가입 구현

---
+++ springBoot/jwtlogin/jwtlogin/src/main/java/com/example/jwtlogin/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package com.example.jwtlogin.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    private String role;    // RoLE_USER, ROLE_ADMIN 등
+}
